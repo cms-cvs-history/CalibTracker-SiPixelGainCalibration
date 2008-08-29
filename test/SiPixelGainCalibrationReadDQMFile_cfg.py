6d285b4761169a287de9dec2703f7bd6c4ce4df9
cmsbuild <>
2008-08-29 14:59:28 +0000

    This commit was manufactured by cvs2git to create tag 'CalibTracker- SiPixelGainCalibration-V00-02-01'.
    Sprout from master 2007-12-06 17:30:23 UTC Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch> 'modified version, included database output possibility (configurable by bool flag in configuration file)'
    Cherrypick from master 2008-08-29 14:59:27 UTC Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch> 'new version of gain calibration. Includes conversion from VCalhHigh to VCalLow and CORRECT function of gain for database input:':
        python/SiPixelCalibDigiProducer_cfi.py
        python/SiPixelGainCalibrationAnalysis_cfi.py
        test/SealModules.cc
        test/SiPixelGainCalibrationReadDQMFile.cc
        test/SiPixelGainCalibrationReadDQMFile.cfg
        test/SiPixelGainCalibrationReadDQMFile.h
        test/SiPixelGainCalibrationReadDQMFile_cfg.py
        test/SimpleTestPrintOutPixelCalibAnalyzer.h
    Cherrypick from master 2008-02-14 21:23:11 UTC Freya Blekman <sha1-d6b3d9f0d38153e2cc18370af560bd9f53546784@cern.ch> 'migration of SiPixelCalibConfiguration object to CondFormats':
        test/sipixelgainanalysis_fromRawSLinkData.cfg

---
+++ test/SiPixelGainCalibrationReadDQMFile_cfg.py
@@ -0,0 +1,56 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("test")
+process.load("CondTools.SiPixel.SiPixelGainCalibrationService_cfi")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Geometry.TrackerGeometryBuilder.trackerGeometry_cfi")
+process.load("Geometry.TrackerGeometryBuilder.idealForDigiTrackerGeometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "STARTUP_V4::All"
+
+process.readfileOffline = cms.EDFilter("SiPixelGainCalibrationReadDQMFile",
+    inputrootfile = cms.untracked.string('DQM_GainCalibration_Run54974.root'),
+    record = cms.untracked.string('SiPixelGainCalibrationOfflineRcd'),
+    useMeanWhenEmpty = cms.untracked.bool(True)                                     
+)
+
+process.readfileHLT = cms.EDFilter("SiPixelGainCalibrationReadDQMFile",
+    inputrootfile = cms.untracked.string('DQM_GainCalibration_Run54974.root'),
+    record = cms.untracked.string('SiPixelGainCalibrationForHLTRcd'),
+    useMeanWhenEmpty = cms.untracked.bool(True)                      
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+process.source = cms.Source("EmptyIOVSource",
+    lastRun = cms.untracked.uint32(1),
+    timetype = cms.string('runnumber'),
+    firstRun = cms.untracked.uint32(1),
+    interval = cms.uint32(1)
+)
+
+process.PoolDBOutputService = cms.Service("PoolDBOutputService",
+    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+    DBParameters = cms.PSet(
+        messageLevel = cms.untracked.int32(10),
+        authenticationPath = cms.untracked.string('.')
+#        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+    ),
+    toPut = cms.VPSet(
+        cms.PSet(
+            record = cms.string('SiPixelGainCalibrationForHLTRcd'),
+            tag = cms.string('GainCalib_TESTHLT2')
+        ), 
+        cms.PSet(
+            record = cms.string('SiPixelGainCalibrationOfflineRcd'),
+            tag = cms.string('GainCalib_TESTOFFLINE2')
+        )
+    ),
+    connect = cms.string('sqlite_file:prova_rawdata.db')
+#    connect = cms.string('oracle://cms_orcoff_int2r/CMS_COND_21X_PIXEL')
+)
+
+process.p = cms.Path(process.readfileHLT*process.readfileOffline)
+
+
